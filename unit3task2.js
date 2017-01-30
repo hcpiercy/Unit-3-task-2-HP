1f865407aa24ecb447c9137cfa4e089f6f1eb72f
hcpiercy <harrietzw@gmail.com>
2017-01-30 15:03:24 +0000

    Add files via upload

---
+++ unit3task2.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+var = bumsup;
+
+function preload() {
+  bumsup = loadImage('BUMSUPFINALLOGO.png');
+} 
+
+function setup() {
+  image(bumsup, 5, 5);
+}
+
+function draw () {
+background (30, 150, 0);
+}
+
